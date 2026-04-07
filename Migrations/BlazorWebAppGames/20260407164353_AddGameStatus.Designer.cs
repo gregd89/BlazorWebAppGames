b3d4ed7803eecbb28bcabe729b7782203198ca03
Greg Dunne <gregd89@users.noreply.github.com>
2026-04-07 17:48:20 +0100

    Added game status to database

--- Migrations/BlazorWebAppGames/20260407164353_AddGameStatus.Designer.cs
+++ Migrations/BlazorWebAppGames/20260407164353_AddGameStatus.Designer.cs
@@ -4,6 +4,7 @@ using BlazorWebAppGames.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorWebAppGames.Migrations.BlazorWebAppGames
 {
     [DbContext(typeof(BlazorWebAppGamesContext))]
-    partial class BlazorWebAppGamesContextModelSnapshot : ModelSnapshot
+    [Migration("20260407164353_AddGameStatus")]
+    partial class AddGameStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
